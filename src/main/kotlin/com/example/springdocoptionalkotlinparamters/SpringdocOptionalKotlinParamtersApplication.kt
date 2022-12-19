fe747898ee29378b17551fb32f770f8eb3f2fce9
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2022-12-19 07:55:20 +0100

    Generate project skeleton

---
+++ src/main/kotlin/com/example/springdocoptionalkotlinparamters/SpringdocOptionalKotlinParamtersApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springdocoptionalkotlinparamters
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringdocOptionalKotlinParamtersApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringdocOptionalKotlinParamtersApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springdocoptionalkotlinparamters/SpringdocOptionalKotlinParamtersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springdocoptionalkotlinparamters
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringdocOptionalKotlinParamtersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
