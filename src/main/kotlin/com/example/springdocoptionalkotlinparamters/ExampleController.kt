b05a31bee07bc7c68432d668ee481a98c6af514e
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2022-12-19 09:35:09 +0100

    Register controller bean by using a proxy

---
+++ src/main/kotlin/com/example/springdocoptionalkotlinparamters/ExampleController.kt
@@ -0,0 +1,13 @@
+package com.example.springdocoptionalkotlinparamters
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+data class Greeting(val greeting: String)
+
+@RestController
+interface ExampleController {
+    @GetMapping("/")
+    fun greet(@RequestParam name: String?): Greeting
+}
--- src/main/kotlin/com/example/springdocoptionalkotlinparamters/SpringdocOptionalKotlinParamtersApplication.kt
+++ src/main/kotlin/com/example/springdocoptionalkotlinparamters/SpringdocOptionalKotlinParamtersApplication.kt
@@ -1,10 +1,25 @@
 package com.example.springdocoptionalkotlinparamters
 
+import org.springframework.aop.framework.ProxyFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.support.GenericApplicationContext
 
 @SpringBootApplication
-class SpringdocOptionalKotlinParamtersApplication
+class SpringdocOptionalKotlinParamtersApplication {
+    @Bean
+    fun controller(applicationContext: GenericApplicationContext): ExampleController {
+        return createProxy(ExampleController::class.java)
+    }
+
+    private fun <T> createProxy(clazz: Class<T>): T {
+        val proxyFactory = ProxyFactory(clazz)
+        proxyFactory.targetClass = clazz
+        @Suppress("UNCHECKED_CAST")
+        return proxyFactory.proxy as T
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringdocOptionalKotlinParamtersApplication>(*args)
