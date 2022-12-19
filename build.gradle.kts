a1b00f543a85ab4787b85c9bfd8976157b4a2c2b
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2022-12-19 09:33:31 +0100

    Add springdoc as a dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.13")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
